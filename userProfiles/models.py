536d8d2666b2909f46bab625c81d07bcf03defc4
nad1ra <nodirakamaliddinovna@gamil.com>
2025-04-07 18:25:30 +0500

    not finished

---
+++ userProfiles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile_pics')
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.name
